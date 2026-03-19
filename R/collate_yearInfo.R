4ea5a048928720a03641d92ef53bbd000b7d796d
ChloeRNater <chloe.nater@nina.no>
2026-03-19 13:09:16 +0100

    Adjusted function for presenting time overview

--- R/collate_yearInfo.R
+++ R/collate_yearInfo.R
@@ -4,7 +4,7 @@
 #' @param Tmax integer. The number of years to consider in analyses.
 #'
 #' @return a dataframe containing year indices as used in the model with 
-#' corresponding reproduction years and winter/summer harvest seasons. 
+#' corresponding reproduction years and winter harvest seasons. 
 #' This dataframe is not required to run the analyses but the information is
 #' useful for understanding and interpreting model and results. 
 #' @export
@@ -17,8 +17,7 @@ collate_yearInfo <- function(minYear, Tmax){
   YearInfo <- data.frame(
     index = 1:Tmax, # year index as used in model
     ReproductionYear = (1:Tmax) + minYear - 1, # reproduction year
-    WinterHarvestSeason = paste0("Oct ", (1:Tmax) + minYear - 1, " - May ", (1:Tmax) + minYear), # winter harvest season
-    SummerHarvestSeason = paste0("Jul ", (1:Tmax) + minYear - 1, " - Sep ", (1:Tmax) + minYear - 1)) 
+    WinterHarvestSeason = paste0("Oct ", (1:Tmax) + minYear - 1, " - May ", (1:Tmax) + minYear)) 
 
   return(YearInfo)
 }
